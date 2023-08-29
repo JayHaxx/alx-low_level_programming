8a1449bcb16a3892e555b5a6aee7a3e73f3ffd71
JayHaxx <thitojeffrey@gmail>
2023-08-29 04:14:36 -0700

    fix 3 more linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,7 @@
 * >h - is the pointer to the header of the list
 *
 * Return:nnumber of nodes in the list.
-**/
+*/
 size_t print_listint(const listint_t *h)
 {
 size_t num = 0;
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,7 @@
  * >n: data to insert on that new node
  *
  * Return: pointer to the new node, or NULL if it fails
- **/
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
